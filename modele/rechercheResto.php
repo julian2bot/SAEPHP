e57417dc939c2f3dd7ef27a2454e985367a721b9
julian2bot <marquesjulian26@gmail.com>
2025-02-10 15:55:26 +0100

    edit bd tout en min

--- modele/rechercheResto.php
+++ modele/rechercheResto.php
@@ -4,5 +4,9 @@ require_once "../utils/BD/requettes/select.php";
 
 
 if(isset($_POST["nomRestoCuisine"])){
+    // echo $_POST["nomRestoCuisine"];
     echo json_encode(rechercheResto($bdd, $_POST["nomRestoCuisine"]));
+}
+else{
+    echo json_encode(array());
 }
